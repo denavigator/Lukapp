30a8292d24a72241b367839264e0b2df5f9dc3ac
denavigator <asumanlukomwa001@gmail.com>
2026-02-05 09:11:36 +0300

    Create page.js

---
+++ app/terms/page.js
@@ -0,0 +1,8 @@
+export default function Terms() {
+  return (
+    <div>
+      <h1>Terms of Use</h1>
+      <p>Use the services responsibly. payments are required via Whatsapp. WhatsApp contact only.</p>
+    </div>
+  )
+    }
