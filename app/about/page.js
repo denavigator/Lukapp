4a7ffd23cf42bb968131a06497d9e1ba4a962db4
denavigator <asumanlukomwa001@gmail.com>
2026-02-05 09:10:01 +0300

    Create page.js

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About LUKTECH Hub</h1>
+      <p>We provide free tech services, bots support, and web help via WhatsApp.</p>
+    </div>
+  )
+    }
