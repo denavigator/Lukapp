5c902de3cbd3436f0ddf2aea0502e59a34aab8ca
denavigator <asumanlukomwa001@gmail.com>
2026-02-05 09:13:07 +0300

    Create page.js

---
+++ app/privacy/page.js
@@ -0,0 +1,8 @@
+export default function Privacy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>No personal data is collected. All services are paid got via Whatsapp and WhatsApp-based.</p>
+    </div>
+  )
+}
